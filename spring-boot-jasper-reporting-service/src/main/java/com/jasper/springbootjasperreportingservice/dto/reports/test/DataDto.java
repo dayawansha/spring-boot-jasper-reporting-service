65d92d2975b3a1f58b923441d896020b1219325e
dushman <dushman.d@informaticsint.com>
2019-11-11 23:57:07 +0530

    project is running.

---
+++ spring-boot-jasper-reporting-service/src/main/java/com/jasper/springbootjasperreportingservice/dto/reports/test/DataDto.java
@@ -0,0 +1,17 @@
+package com.jasper.springbootjasperreportingservice.dto.reports.test;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class DataDto {
+    private Integer valueOne;
+    private String valueTwo;
+    private Double valueThree;
+    private Boolean valueFour;
+}
--- spring-boot-jasper-reporting-service/src/main/java/com/jasper/springbootjasperreportingservice/dto/reports/test/TestReportData.java
+++ spring-boot-jasper-reporting-service/src/main/java/com/jasper/springbootjasperreportingservice/dto/reports/test/TestReportData.java
@@ -4,10 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.ArrayList;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class TestReportData {
     private String reportType;
+
+    private String stringValue;
+    private Double doubleValue;
+
+    private String[] stringArray;
+    private Integer[] integerArray;
+
+    private ArrayList<String> stringArrayList;
+    private ArrayList<DataDto> dataDtoArrayListArrayList;
 }
