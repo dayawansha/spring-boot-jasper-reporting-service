250fec2721f57f5084f4f8c58e1da187b53f35c2
dushman <dushman.d@informaticsint.com>
2019-11-11 11:54:04 +0530

    first initial commit.

---
+++ src/main/java/com/jasper/springbootjasperreportingservice/SpringBootJasperReportingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jasper.springbootjasperreportingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJasperReportingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJasperReportingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jasper/springbootjasperreportingservice/SpringBootJasperReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasper.springbootjasperreportingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJasperReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
