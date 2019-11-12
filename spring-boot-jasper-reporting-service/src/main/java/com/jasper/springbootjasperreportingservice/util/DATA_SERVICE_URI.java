791cd0769002bb9e3a39fc6f22106ca830604e7f
dushman <dushman.d@informaticsint.com>
2019-11-12 21:53:17 +0530

    project is running.

--- spring-boot-jasper-reporting-service/src/main/java/com/jasper/springbootjasperreportingservice/util/DATA_SERVICE_URI.java
+++ spring-boot-jasper-reporting-service/src/main/java/com/jasper/springbootjasperreportingservice/util/DATA_SERVICE_URI.java
@@ -1,24 +1,10 @@
 package com.jasper.springbootjasperreportingservice.util;
 
-
-import org.apache.catalina.core.ApplicationContext;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
-
-import javax.annotation.Resource;
-import javax.inject.Inject;
-
-@Configuration
-@PropertySource("classpath:application.properties")
 public class DATA_SERVICE_URI {
 
 
     public  static final String baseURL = "/dataService/";
 
     public  static final String basicReport = baseURL + "basicReport";
-
-
+    
 }
