24c5cb76c8140c11836bb25c5dc4a0816edb109e
dushman <dushman.d@informaticsint.com>
2019-11-12 21:52:39 +0530

    data service url has changed.

--- spring-boot-jasper-reporting-service/src/main/java/com/jasper/springbootjasperreportingservice/util/DATA_SERVICE_URI.java
+++ spring-boot-jasper-reporting-service/src/main/java/com/jasper/springbootjasperreportingservice/util/DATA_SERVICE_URI.java
@@ -1,20 +1,24 @@
 package com.jasper.springbootjasperreportingservice.util;
 
 
+import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.Resource;
+import javax.inject.Inject;
+
+@Configuration
+@PropertySource("classpath:application.properties")
 public class DATA_SERVICE_URI {
 
-    @Autowired
-    private static Environment environment;
-
-    public static final String origin = environment.getProperty("jasper.report.origin");
 
     public  static final String baseURL = "/dataService/";
 
-
-    public  static final String testReport = origin + baseURL + "testReport";
+    public  static final String basicReport = baseURL + "basicReport";
 
 
 }
